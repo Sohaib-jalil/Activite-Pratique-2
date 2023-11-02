6004751e7ca43c1553e6f7de23ed93e89c42c037
Sohaib-jalil <jalil.sohaib01@gmail.com>
2023-11-02 23:52:45 +0100

    V2.0 graphQL

---
+++ src/main/java/org/sid/banckaccountservice/web/BankAccountGraphQLController.java
@@ -0,0 +1,53 @@
+package org.sid.banckaccountservice.web;
+
+import org.sid.banckaccountservice.dto.BankAccountRequestDTO;
+import org.sid.banckaccountservice.dto.BankAccountResponseDTO;
+import org.sid.banckaccountservice.entities.BankAccount;
+import org.sid.banckaccountservice.repositories.BankAccountRepository;
+import org.sid.banckaccountservice.services.AccountService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.MutationMapping;
+import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.stereotype.Controller;
+
+import java.util.List;
+
+@Controller
+public class BankAccountGraphQLController {
+    @Autowired
+    private BankAccountRepository bankAccountRepository;
+    @Autowired
+    private AccountService accountService;
+
+    @QueryMapping
+    public List<BankAccount> accountList(){
+        return bankAccountRepository.findAll();
+    }
+
+    @QueryMapping
+    public BankAccount bankAccountById(@Argument String id){
+        return bankAccountRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException(String.format("Account %s not found", id)));
+    }
+
+    @MutationMapping
+    public BankAccountResponseDTO addAccount(@Argument BankAccountRequestDTO bankAccount){
+        return accountService.addAccount(bankAccount);
+    }
+
+    @MutationMapping
+    public BankAccountResponseDTO updateAccount(@Argument String id, @Argument BankAccountRequestDTO bankAccount){
+        return accountService.updateAccount(id, bankAccount);
+    }
+
+    @MutationMapping
+    public Boolean deleteAccount(@Argument String id){
+        bankAccountRepository.deleteById(id);
+        return true;
+    }
+}
+/*
+record BankAccountDTO(Double balance, String type, String currency){
+}
+*/
